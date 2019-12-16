1a55aaf2546baec0d14867ca07fa30738bf6fcad
Ray Zane <raymondzane@gmail.com>
2019-12-16 17:13:43 -0500

    Generate documentation

--- lib/sdl/name.rb
+++ lib/sdl/name.rb
@@ -2,21 +2,25 @@ require "active_support/inflector"
 
 module SDL
   # An extension of a string that will format a name
-  # This is especially useful for code generation
   class Name < String
-    # @api private
-    def self.inflect(name, transforms)
-      define_method(name) do
-        transforms.reduce(self) do |acc, arg|
-          case arg
-          when :upcase
-            acc.upcase
-          when :lower_camelize
-            ActiveSupport::Inflector.camelize(acc, false)
-          when :lower_humanize
-            ActiveSupport::Inflector.humanize(acc, capitalize: false)
-          else
-            ActiveSupport::Inflector.send(arg, acc)
+    class << self
+      private
+
+      # @!macro [attach] inflect
+      #   @return [String]
+      def inflect(name, transforms)
+        define_method(name) do
+          transforms.reduce(self) do |acc, arg|
+            case arg
+            when :upcase
+              acc.upcase
+            when :lower_camelize
+              ActiveSupport::Inflector.camelize(acc, false)
+            when :lower_humanize
+              ActiveSupport::Inflector.humanize(acc, capitalize: false)
+            else
+              ActiveSupport::Inflector.send(arg, acc)
+            end
           end
         end
       end
